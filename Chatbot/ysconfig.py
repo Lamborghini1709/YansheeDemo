12499c5570a58e819e68e42d684077dc3ae26c66
Lamborghini1709 <32261121+Lamborghini1709@users.noreply.github.com>
2024-06-03 02:48:29 +0000

    Add files via upload

---
+++ Chatbot/ysconfig.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2024/6/3 10:31
+# @Author  : wayne
+# @File    : Config.py
+# @Software: PyCharm
+
+FIRST_YANSHEE_IF = '192.168.2.64'
+SECOND_YANSHEE_IF = '192.168.2.109'
